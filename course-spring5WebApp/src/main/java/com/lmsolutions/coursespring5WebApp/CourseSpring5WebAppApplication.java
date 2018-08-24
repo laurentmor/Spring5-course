5fc4f6374c07af766f39f2676bb648d915b7eaa3
Laurent Morissette <laurent.morissette@gmail.com>
2018-08-24 15:11:51 -0400

    Initial commit

---
+++ course-spring5WebApp/src/main/java/com/lmsolutions/coursespring5WebApp/CourseSpring5WebAppApplication.java
@@ -0,0 +1,12 @@
+package com.lmsolutions.coursespring5WebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpring5WebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSpring5WebAppApplication.class, args);
+	}
+}
