96c7a87cb7685a0042ad32bbf9b1b1ff0cc62bb9
Laurent Morissette <laurent.morissette@gmail.com>
2018-08-25 11:27:46 -0400

    introduced correcy schema

--- course-spring5WebApp/src/main/java/com/lmsolutions/coursespring5WebApp/CourseSpring5WebAppApplication.java
+++ course-spring5WebApp/src/main/java/com/lmsolutions/coursespring5WebApp/CourseSpring5WebAppApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CourseSpring5WebAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CourseSpring5WebAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CourseSpring5WebAppApplication.class, args);
+    }
 }
