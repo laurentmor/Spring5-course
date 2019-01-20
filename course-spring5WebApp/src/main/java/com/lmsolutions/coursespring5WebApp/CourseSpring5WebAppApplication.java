d971c69aa9f913254aabb1e4dd721aeb7ca39092
Laurent Morissette <laurent.morissette@gmail.com>
2019-01-20 11:25:55 -0500

    submodules update

--- course-spring5WebApp/src/main/java/com/lmsolutions/coursespring5WebApp/CourseSpring5WebAppApplication.java
+++ course-spring5WebApp/src/main/java/com/lmsolutions/coursespring5WebApp/CourseSpring5WebAppApplication.java
@@ -3,10 +3,16 @@ package com.lmsolutions.coursespring5WebApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Set;
+
 @SpringBootApplication
 public class CourseSpring5WebAppApplication {
 
+
     public static void main(String[] args) {
-        SpringApplication.run(CourseSpring5WebAppApplication.class, args);
+		Set.of(1,2,3,4,5,6,7,8,9,10);
+    	SpringApplication.run(CourseSpring5WebAppApplication.class, args);
     }
 }
